d62c80d711f7522752494e829fc47b7e856296fb
madhavbhatt <madhav.bhatt73@gmail.com>
2018-03-10 11:01:03 -0500

    c2

--- desi_C2/desi_C2/settings.py
+++ desi_C2/desi_C2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tbxvi+&tzlf@y%gy4b8&)zr+!*=2uc8!^189^--dx%eqyh36kr'
+SECRET_KEY = file('/etc/secret_key_desi_c2.txt').read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
